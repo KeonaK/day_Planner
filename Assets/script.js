46310ebedf3d0450f1e4acf5cc3f90cf4b6d85f7
Keona Kirby <keonakirby@gmail.com>
2021-03-25 15:49:26 -0500

    added a function that changes color according to the current time

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,51 @@
+
+let saveBtnEl = document.getElementsByClassName("saveBtn");
 //sets the date 
-let $dateDisplayEl = document.getElementById("currentDay");
+let dateDisplayEl = document.getElementById("currentDay");
  let today = moment().format(" dddd, MMMM Do YYYY");
- $dateDisplayEl.textContent = today;
+ dateDisplayEl.textContent = today;
+//gets the hour 
+ let currentTime = moment().hour();
+
+
+
+
+
+//function for color coded time block
+$(".description").each(function(){
+
+   let clock = $(this).data("time");
+   console.log(clock);
+
+
+    if(clock < currentTime){
+        //this refers to whatever object it is contextually related to 
+        $(this).addClass("past");
+     }
+     else if (clock === currentTime){
+         $(this).addClass("present");
+     }
+     else{
+         $(this).addClass("future");
+     }
+    
+});
+
+
+
+
+
+
+
+
+
+//save button
+//give data- attribute to the text area and assing it and have the button save that value 
+//  function saveButton (){
+
+     
+//  }
+
+//  saveBtnEl.addEventListener("click",saveButton);
 
 
